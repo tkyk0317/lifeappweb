c7a74d36122a1c51b79ecac4b6b42e5e9ce5d1b7
takayuki tanaka <takayuki.1981.0317@gmail.com>
2017-02-24 16:38:57 +0900

    [add] エラーページ内にリンク追加.
    エラーページへ内に、Signin/Signupリンクを追加.
    デザインは見直す必要あり.

--- node/views/error.jsx
+++ node/views/error.jsx
@@ -7,9 +7,17 @@ import DefaultLayout from './layout';
 var Error = React.createClass({
   render: function() {
     return (
-      <DefaultLayout title="Error">
-        <h1>Not Found Page</h1>
-      </DefaultLayout>
+        <DefaultLayout title="Error">
+            <div id="error">
+                <div id="content">
+                    <h1>404 page not found</h1>
+                    <br />
+                    <div id="recovery"><a href="/signin">Signin</a></div>
+                    <div id="recovery"><a href="/signup">Signup</a></div>
+                </div>
+            </div>
+            <link type="text/css" rel="stylesheet" href="/stylesheets/error.css" />
+        </DefaultLayout>
     );
   }
 });
